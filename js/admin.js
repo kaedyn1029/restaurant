5411fd83868d2f5d8e34190cc986d8e5215135e5
Mark Hilton <mdhilton221@stevenscollege.edu>
2022-12-08 13:18:19 -0500

    added loding menu image

--- js/admin.js
+++ js/admin.js
@@ -25,16 +25,34 @@ function add_section(){
 
 function add_item(caller){
     let div=document.createElement("div")
-    let img=document.createElement("input")
+    let img_up=document.createElement("input")
+    let img_dat=document.createElement("img")
     let about=document.createElement("input")
     let helth=document.createElement("input")
     let price=document.createElement("input")
 
-    img.type="file"
-    div.appendChild(img)
+    img_up.type="file"
+    img_up.addEventListener("change",()=>{change_menu_img(img_up)})
+    img_dat.src=""
+    div.appendChild(img_dat)
+    div.appendChild(img_up)
     div.appendChild(about)
     div.appendChild(helth)
     div.appendChild(price)
-
     caller.parentElement.appendChild(div)
+}
+
+function change_menu_img(bttn){
+    look=bttn.parentElement.children[0]
+    const reader = new FileReader();
+    reader.addEventListener("load", ()=>{look.src=reader.result})
+    reader.readAsDataURL(bttn.files[0])
+}
+
+function save(){
+    
+}
+
+function load(){
+
 }
