8252a5d9bb35571916a2dee5261852abb21d2f27
kaedyn <kckessler221@stevenscollege.edu>
2022-12-12 15:34:40 -0500

    added login page functionality

--- js/login.js
+++ js/login.js
@@ -1,4 +1,20 @@
 let login_btn = document.getElementById("btn");
+let email = document.getElementById("email");
+let password = document.getElementById("pass");
 function login(){
-
+     if((email !== "") || (password !== "")){
+         let user = get_user(email.value);
+         if(user !== null){
+             let real_pass = user.salt;
+             if(real_pass == salt(pass.value)){
+                 console.log("Verified");
+             } else {
+                 console.log("Unverified");
+             }
+         } else {
+             console.log("User does not exist");
+         }
+     } else {
+         console.log("Input fields left blank");
+     }
 }
